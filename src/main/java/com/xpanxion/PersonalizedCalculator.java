076220a89d3d91ec2acb71cb9fdf9ac9474fcfb1
brettalanmeyer <brettalanmeyer@gmail.com>
2015-04-09 12:48:15 -0500

    My first Java App

---
+++ src/main/java/com/xpanxion/PersonalizedCalculator.java
@@ -0,0 +1,10 @@
+package com.xpanxion;
+
+public class PersonalizedCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Personalized Calcaultor");
+
+    }
+}
