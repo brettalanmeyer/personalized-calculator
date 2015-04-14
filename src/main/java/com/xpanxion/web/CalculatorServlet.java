1079b2cdcc7d885b01d55c88d1f4cf81a8317060
brettalanmeyer <brettalanmeyer@gmail.com>
2015-04-14 11:51:36 -0500

    Refactored servlet to use JSP views

--- src/main/java/com/xpanxion/web/CalculatorServlet.java
+++ src/main/java/com/xpanxion/web/CalculatorServlet.java
@@ -1,7 +1,6 @@
 package com.xpanxion.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,21 +12,7 @@ public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String name = req.getParameter("guest");
-
-        PrintWriter writer = resp.getWriter();
-
-        writer.write("<html>");
-
-        writer.write("<head>");
-        writer.write("<title>Personalized Calculator</title>");
-        writer.write("</head>");
-
-        writer.write("<body>");
-        writer.write("<h1>Hi, " + name + "</h1>");
-        writer.write("</body>");
-
-        writer.write("</html>");
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
 
     }
 
---
+++ src/main/webapp/calculator.jsp
@@ -0,0 +1,17 @@
+<% String name = request.getParameter("guest"); %>
+<!doctype html>
+<html>
+
+	<head>
+		
+		<title><%= name %>&#39;s Personalized Calculator</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/all.css">
+	
+	<body>
+	
+		<h1>Hi, <%= name %></h1>
+		
+	</body>
+	
+</html>
