3e9f7fe8f5775f2749385bf87ada30bb5fad4dc4
brettalanmeyer <brettalanmeyer@gmail.com>
2015-04-14 12:22:31 -0500

    Added JSTL and calculator functionality

--- src/main/java/com/xpanxion/web/CalculatorServlet.java
+++ src/main/java/com/xpanxion/web/CalculatorServlet.java
@@ -1,16 +1,43 @@
 package com.xpanxion.web;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.xpanxion.Calculation;
+import com.xpanxion.Calculator;
 
 public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        String guestName = req.getParameter("guest");
+        session.setAttribute("guest", guestName);
+
+        req.getRequestDispatcher("calculator.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+        String guestName = (String) session.getAttribute("guest");
+
+        String operands = req.getParameter("operands");
+        Scanner scanner = new Scanner(operands);
+
+        Calculator calculator = new Calculator();
+        Calculation results = calculator.performAction(Calculator.ADDITION, guestName, scanner);
+
+        req.setAttribute("resultText", results.getTextResult());
 
         req.getRequestDispatcher("calculator.jsp").forward(req, resp);
 
--- src/main/webapp/calculator.jsp
+++ src/main/webapp/calculator.jsp
@@ -1,16 +1,36 @@
-<% String name = request.getParameter("guest"); %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="name" value="${sessionScope.guest}" />
 <!doctype html>
 <html>
 
 	<head>
 		
-		<title><%= name %>&#39;s Personalized Calculator</title>
+		<title>${name}&#39;s Personalized Calculator</title>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/all.css">
 	
 	<body>
 	
-		<h1>Hi, <%= name %></h1>
+		<h1>Hi, ${name}</h1>
+		
+		<form method="GET" action="/calculator">
+		
+			<div class="form-group">
+				<label for="form-field-operands">Please enter some numbers (separated by spaces)</label>
+				<input type="text" id="form-field-operands" name="operands" required />
+			</div>
+			
+			<div class="form-group">
+				<button type="submit">Calculate</button>
+			</div>
+		
+		</form>
+		
+		<div class="form-group">
+			${requestScope.resultText}
+		</div>
+		
+		<a href="/logout">Log Out</a>
 		
 	</body>
 	
