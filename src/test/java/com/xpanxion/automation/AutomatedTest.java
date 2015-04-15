55002cf735f8a3db7f12780b4e2e094b0c3007f1
brettalanmeyer <brettalanmeyer@gmail.com>
2015-04-15 17:02:48 -0500

    First automated test

--- src/test/java/com/xpanxion/automation/AutomatedTest.java
+++ src/test/java/com/xpanxion/automation/AutomatedTest.java
@@ -1,7 +1,13 @@
 package com.xpanxion.automation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutomatedTest {
 
@@ -11,10 +17,27 @@ public class AutomatedTest {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://www.reddit.com");
+        driver.get("http://localhost:8080/index.html");
 
         String pageTitle = driver.getTitle();
         System.out.println(pageTitle);
+
+        WebElement guestField = driver.findElement(By.name("guest"));
+        guestField.sendKeys("Brett");
+        guestField.submit();
+
+        ExpectedCondition<Boolean> pageIsLoaded = new ExpectedCondition<Boolean>() {
+
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        wait.until(pageIsLoaded);
+
+        WebElement greeting = driver.findElement(By.xpath("/html/body/h1"));
+        System.out.println(greeting.getText());
 
         driver.quit();
 
